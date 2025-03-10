9257224e12525d4187f306e7d58c16837ae0df98
mateoramirez24 <mateo.ramirez@iac.com.co>
2025-03-10 08:01:15 -0500

    SOME CORRECTIONS

--- src/ml/models/shared/directional_prediction.py
+++ src/ml/models/shared/directional_prediction.py
@@ -4,15 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
-
-# -------------------------------------------------------------------------------------------
-# Predict directions
-# -------------------------------------------------------------------------------------------
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 
 def calculate_directions(data):
-<<<<<<< Updated upstream
-=======
     """
     Calcula las direcciones de cambio en los datos de entrada (Open, Close).
     """
@@ -29,13 +23,6 @@ def calculate_directions(data):
                 directional_data[i, j] = [1, 0]  # Down
 
     """
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     directions = np.diff(data.squeeze())
     directional_data = np.zeros((directions.size, 2))
     for i, diff in enumerate(directions):
@@ -46,70 +33,59 @@ def calculate_directions(data):
     return directional_data
 
 def calculate_movement_metrics(true_labels, predicted_labels, model_name, set_type, data_type):
-    if set_type == "Train":
-        # Calculate only accuracy for the training set
-        accuracy = accuracy_score(true_labels, predicted_labels) * 100
-        print(f'{model_name} ({data_type}) | Train Accuracy: {accuracy:.2f}%')
+    """
+    Calcula métricas de clasificación (accuracy, recall, precision, F1-score) 
+    y muestra la matriz de confusión si es el set de test.
+    """
+    metrics = {}
 
-<<<<<<< Updated upstream
-        return {'Train Accuracy': accuracy}
-=======
     # Calcular métricas para cada feature por separado
     """
     Para predecir open y close
     for i, feature in enumerate(["Open", "Close"]): 
         acc = accuracy_score(true_labels[:, i], predicted_labels[:, i]) * 100
         metrics[f'{set_type} Accuracy ({feature})'] = acc
->>>>>>> Stashed changes
 
-    if set_type == "Val":
-        # Calculate only accuracy for the validation set
-        accuracy = accuracy_score(true_labels, predicted_labels) * 100
-        print(f'{model_name} ({data_type}) | Validation Accuracy: {accuracy:.2f}%')
+        if set_type == "Test":
+            recall = recall_score(true_labels[:, i], predicted_labels[:, i], pos_label=1) * 100
+            precision_rise = precision_score(true_labels[:, i], predicted_labels[:, i], pos_label=1) * 100
+            precision_fall = precision_score(true_labels[:, i], predicted_labels[:, i], pos_label=0) * 100
+            f1 = f1_score(true_labels[:, i], predicted_labels[:, i], pos_label=1) * 100
 
-        return {'Validation Accuracy': accuracy}
 
-    elif set_type == "Test":
-        # Calculate metrics for the test set
-        accuracy = accuracy_score(true_labels, predicted_labels) * 100
-        recall = recall_score(true_labels, predicted_labels, pos_label=1) * 100
-        precision_rise = precision_score(true_labels, predicted_labels, pos_label=1) * 100
-        precision_fall = precision_score(true_labels, predicted_labels, pos_label=0) * 100
-        f1 = f1_score(true_labels, predicted_labels, pos_label=1) * 100
+            print(f'{model_name} ({data_type}) | Test Accuracy: {acc:.2f}%')
+            print(f'  - Recall ({feature}): {recall:.2f}%')
+            print(f'  - Precision (Rise) ({feature}): {precision_rise:.2f}%')
+            print(f'  - Precision (Fall) ({feature}): {precision_fall:.2f}%')
+            print(f'  - F1 Score ({feature}): {f1:.2f}%')
 
-        print(f'{model_name} ({data_type}) | Test Accuracy: {accuracy:.2f}%')
-        print(f'{model_name} ({data_type}) | Recall: {recall:.2f}%')
-        print(f'{model_name} ({data_type}) | Precision (Rise): {precision_rise:.2f}%')
-        print(f'{model_name} ({data_type}) | Precision (Fall): {precision_fall:.2f}%')
-        print(f'{model_name} ({data_type}) | F1 Score: {f1:.2f}%')
+            metrics.update({
+                f'Recall ({feature})': recall,
+                f'Precision (Rise) ({feature})': precision_rise,
+                f'Precision (Fall) ({feature})': precision_fall,
+                f'F1 Score ({feature})': f1
+            })
 
-        # Confusion matrix
-        cm = confusion_matrix(true_labels, predicted_labels)
-        cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            # Matriz de confusión
+            cm = confusion_matrix(true_labels[:, i], predicted_labels[:, i])
+            cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
-        plt.figure(figsize=(10, 7))
-        sns.heatmap(cm, annot=True, fmt="d", cmap=plt.cm.Blues, xticklabels=["Down", "Up"], yticklabels=["Down", "Up"], cbar=False)
+            plt.figure(figsize=(6, 5))
+            sns.heatmap(cm, annot=True, fmt="d", cmap=plt.cm.Blues, xticklabels=["Down", "Up"], yticklabels=["Down", "Up"], cbar=False)
 
-        # Add percentages
-        for i in range(cm.shape[0]):
-            for j in range(cm.shape[1]):
-                plt.text(j + 0.5, i + 0.55, f'\n({cm_norm[i, j]:.2%})',
-                         horizontalalignment='center',
-                         verticalalignment='center',
-                         color='black',
-                         fontsize=9)
+            # Agregar porcentajes
+            for r in range(cm.shape[0]):
+                for c in range(cm.shape[1]):
+                    plt.text(c + 0.5, r + 0.55, f'\n({cm_norm[r, c]:.2%})',
+                             horizontalalignment='center',
+                             verticalalignment='center',
+                             color='black',
+                             fontsize=9)
 
-<<<<<<< Updated upstream
-=======
             plt.xlabel('Predicted')
             plt.ylabel('True')
             plt.title(f'{model_name} Confusion Matrix ({data_type} Data - {feature})')
             plt.show()
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
     """
 
     if set_type == "Train":
@@ -171,139 +147,35 @@ def calculate_movement_metrics(true_labels, predicted_labels, model_name, set_ty
 
 
 def evaluate_directional_movement(actual_train, pred_train, actual_val, pred_val, actual_test, pred_test, model_name, data_type, using_darts=True):
->>>>>>> Stashed changes
     """
-
-    if set_type == "Train":
-        # Calculate only accuracy for the training set
-        accuracy = accuracy_score(true_labels, predicted_labels) * 100
-        print(f'{model_name} ({data_type}) | Train Accuracy: {accuracy:.2f}%')
-
-        return {'Train Accuracy': accuracy}
-
-    if set_type == "Val":
-        # Calculate only accuracy for the validation set
-        accuracy = accuracy_score(true_labels, predicted_labels) * 100
-        print(f'{model_name} ({data_type}) | Validation Accuracy: {accuracy:.2f}%')
-
-        return {'Validation Accuracy': accuracy}
-
-    elif set_type == "Test":
-        # Calculate metrics for the test set
-        accuracy = accuracy_score(true_labels, predicted_labels) * 100
-        recall = recall_score(true_labels, predicted_labels, pos_label=1) * 100
-        precision_rise = precision_score(true_labels, predicted_labels, pos_label=1) * 100
-        precision_fall = precision_score(true_labels, predicted_labels, pos_label=0) * 100
-        f1 = f1_score(true_labels, predicted_labels, pos_label=1) * 100
-
-        print(f'{model_name} ({data_type}) | Test Accuracy: {accuracy:.2f}%')
-        print(f'{model_name} ({data_type}) | Recall: {recall:.2f}%')
-        print(f'{model_name} ({data_type}) | Precision (Rise): {precision_rise:.2f}%')
-        print(f'{model_name} ({data_type}) | Precision (Fall): {precision_fall:.2f}%')
-        print(f'{model_name} ({data_type}) | F1 Score: {f1:.2f}%')
-
-        # Confusion matrix
-        cm = confusion_matrix(true_labels, predicted_labels)
-        cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-        plt.figure(figsize=(10, 7))
-        sns.heatmap(cm, annot=True, fmt="d", cmap=plt.cm.Blues, xticklabels=["Down", "Up"], yticklabels=["Down", "Up"], cbar=False)
-
-        # Add percentages
-        for i in range(cm.shape[0]):
-            for j in range(cm.shape[1]):
-                plt.text(j + 0.5, i + 0.55, f'\n({cm_norm[i, j]:.2%})',
-                            horizontalalignment='center',
-                            verticalalignment='center',
-                            color='black',
-                            fontsize=9)
-
->>>>>>> Stashed changes
-        plt.xlabel('Predicted')
-        plt.ylabel('True')
-        plt.title(f'{model_name} Confusion Matrix ({data_type} Data)')
-        plt.show()
-<<<<<<< Updated upstream
-
-        return {
-            'Test Accuracy': accuracy,
-            'Recall': recall,
-            'Precision (Rise)': precision_rise,
-            'Precision (Fall)': precision_fall,
-            'F1 Score': f1
-        }
-=======
-
-        return {
-            'Test Accuracy': accuracy,
-            'Recall': recall,
-            'Precision (Rise)': precision_rise,
-            'Precision (Fall)': precision_fall,
-            'F1 Score': f1
-        }
-
->>>>>>> Stashed changes
-
-def evaluate_directional_movement(actual_values_train, backtest_train, actual_values_val, backtest_val, actual_values_test, backtest_test, model_name, data_type, using_darts=True):
+    Evalúa la predicción de direcciones de movimiento para cada feature (Open y Close).
+    """
+    # Extraer valores de los diccionarios si se usa Darts
     if using_darts:
-        train_y = actual_values_train.values()
-        train_predictions = backtest_train.values()
-        val_y = actual_values_val.values()
-        val_predictions = backtest_val.values()
-        test_y = actual_values_test.values()
-        test_predictions = backtest_test.values()
+        train_y = actual_train.values()
+        train_predictions = pred_train.values()
+        val_y = actual_val.values()
+        val_predictions = pred_val.values()
+        test_y = actual_test.values()
+        test_predictions = pred_test.values()
     else:
-        train_y = actual_values_train
-        train_predictions = backtest_train
-        val_y = actual_values_val
-        val_predictions = backtest_val
-        test_y = actual_values_test
-        test_predictions = backtest_test
+        train_y, train_predictions = actual_train, pred_train
+        val_y, val_predictions = actual_val, pred_val
+        test_y, test_predictions = actual_test, pred_test
 
-    # Calculate directions for training set
+    # Calcular direcciones para cada conjunto
     true_directions_train = calculate_directions(train_y)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    predicted_directions_train = calculate_directions(train_predictions)
-
-    # Convert to class labels for training set
-    true_labels_train = np.argmax(true_directions_train, axis=1)
-    predicted_labels_train = np.argmax(predicted_directions_train, axis=1)
-
-    # Calculate directions for validation set
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     if len(train_y) != len(true_directions_train):
         print(len(train_y), len(true_directions_train))
         print("aca esta agregando una creo")
         
     pred_directions_train = calculate_directions(train_predictions)
->>>>>>> Stashed changes
     true_directions_val = calculate_directions(val_y)
-    predicted_directions_val = calculate_directions(val_predictions)
-
-    # Convert to class labels for validation set
-    true_labels_val = np.argmax(true_directions_val, axis=1)
-    predicted_labels_val = np.argmax(predicted_directions_val, axis=1)
-
-    # Calculate directions for test set
+    pred_directions_val = calculate_directions(val_predictions)
     true_directions_test = calculate_directions(test_y)
-    predicted_directions_test = calculate_directions(test_predictions)
+    pred_directions_test = calculate_directions(test_predictions)
 
-<<<<<<< Updated upstream
-    # Convert to class labels for test set
-    true_labels_test = np.argmax(true_directions_test, axis=1)
-    predicted_labels_test = np.argmax(predicted_directions_test, axis=1)
-=======
     # Convertir a etiquetas (0=Down, 1=Up) para cada feature
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
 
     #para 2D es con axis =2
     true_labels_train = np.argmax(true_directions_train, axis=1)
@@ -344,101 +216,14 @@ def evaluate_directional_movement(actual_values_train, backtest_train, actual_va
         "Close": close_values,
         "Close_pred": close_pred_values
     })
->>>>>>> Stashed changes
 
-    #para 2D es con axis =2
-    true_labels_train = np.argmax(true_directions_train, axis=1)
-    predicted_labels_train = np.argmax(pred_directions_train, axis=1)
-    true_labels_val = np.argmax(true_directions_val, axis=1)
-    predicted_labels_val = np.argmax(pred_directions_val, axis=1)
-    true_labels_test = np.argmax(true_directions_test, axis=1)
-    predicted_labels_test = np.argmax(pred_directions_test, axis=1)
-
-    # Crear listas de datos concatenando train, val y test
-    #Para 2D
-    #open_values = [y[0] for y in true_labels_train] + [y[0] for y in true_labels_val] + [y[0] for y in true_labels_test]
-    #close_values = [y[1] for y in true_labels_train] + [y[1] for y in true_labels_val] + [y[1] for y in true_labels_test]
-    close_values = [y for y in true_labels_train] + [y for y in true_labels_val] + [y for y in true_labels_test]
-
-    #open_pred_values = [y[0] for y in predicted_labels_train] + [y[0] for y in predicted_labels_val] + [y[0] for y in predicted_labels_test]
-    #close_pred_values = [y[1] for y in predicted_labels_train] + [y[1] for y in predicted_labels_val] + [y[1] for y in predicted_labels_test]
-    close_pred_values = [y for y in predicted_labels_train] + [y for y in predicted_labels_val] + [y for y in predicted_labels_test]
-
-    # Crear la columna de conjunto (Train, Val, Test)
-    sets = (["Train"] * len(true_labels_train) + 
-            ["Val"] * len(true_labels_val) + 
-            ["Test"] * len(true_labels_test))
-
-    # Crear DataFrame
-    """
-    Para 2D
-    df = pd.DataFrame({
-        "Set": sets,
-        "Open": open_values,
-        "Close": close_values,
-        "Open_pred": open_pred_values,
-        "Close_pred": close_pred_values
-    })
-    """
-    df = pd.DataFrame({
-        "Set": sets,
-        "Close": close_values,
-        "Close_pred": close_pred_values
-    })
->>>>>>> Stashed changes
-
-    #para 2D es con axis =2
-    true_labels_train = np.argmax(true_directions_train, axis=1)
-    predicted_labels_train = np.argmax(pred_directions_train, axis=1)
-    true_labels_val = np.argmax(true_directions_val, axis=1)
-    predicted_labels_val = np.argmax(pred_directions_val, axis=1)
-    true_labels_test = np.argmax(true_directions_test, axis=1)
-    predicted_labels_test = np.argmax(pred_directions_test, axis=1)
-
-    # Crear listas de datos concatenando train, val y test
-    #Para 2D
-    #open_values = [y[0] for y in true_labels_train] + [y[0] for y in true_labels_val] + [y[0] for y in true_labels_test]
-    #close_values = [y[1] for y in true_labels_train] + [y[1] for y in true_labels_val] + [y[1] for y in true_labels_test]
-    close_values = [y for y in true_labels_train] + [y for y in true_labels_val] + [y for y in true_labels_test]
-
-    #open_pred_values = [y[0] for y in predicted_labels_train] + [y[0] for y in predicted_labels_val] + [y[0] for y in predicted_labels_test]
-    #close_pred_values = [y[1] for y in predicted_labels_train] + [y[1] for y in predicted_labels_val] + [y[1] for y in predicted_labels_test]
-    close_pred_values = [y for y in predicted_labels_train] + [y for y in predicted_labels_val] + [y for y in predicted_labels_test]
-
-    # Crear la columna de conjunto (Train, Val, Test)
-    sets = (["Train"] * len(true_labels_train) + 
-            ["Val"] * len(true_labels_val) + 
-            ["Test"] * len(true_labels_test))
-
-    # Crear DataFrame
-    """
-    Para 2D
-    df = pd.DataFrame({
-        "Set": sets,
-        "Open": open_values,
-        "Close": close_values,
-        "Open_pred": open_pred_values,
-        "Close_pred": close_pred_values
-    })
-    """
-    df = pd.DataFrame({
-        "Set": sets,
-        "Close": close_values,
-        "Close_pred": close_pred_values
-    })
->>>>>>> Stashed changes
-
-    # Calculate metrics for training set
+    # Calcular métricas
     metrics_train = calculate_movement_metrics(true_labels_train, predicted_labels_train, model_name, "Train", data_type)
-
-    # Calculate metrics for validation set
     metrics_val = calculate_movement_metrics(true_labels_val, predicted_labels_val, model_name, "Val", data_type)
-
-    # Calculate metrics for test set
     metrics_test = calculate_movement_metrics(true_labels_test, predicted_labels_test, model_name, "Test", data_type)
 
-    # Combine accuracy into the test metrics dictionary
-    metrics_test['Validation Accuracy'] = metrics_val['Validation Accuracy']
-    metrics_test['Train Accuracy'] = metrics_train['Train Accuracy']
+    # Unificar métricas de train y val con test
+    metrics_test.update(metrics_val)
+    metrics_test.update(metrics_train)
 
     return true_labels_test, predicted_labels_test, metrics_test, df
--- src/ml/models/xlstm_ts/logic.py
+++ src/ml/models/xlstm_ts/logic.py
@@ -8,6 +8,7 @@ from ml.models.xlstm_ts.preprocessing import inverse_normalise_data_xlstm
 from ml.constants import SEQ_LENGTH_XLSTM
 from ml.models.xlstm_ts.xlstm_ts_model import create_xlstm_model
 from ml.models.xlstm_ts.training import train_model, evaluate_model
+import numpy as np
 
 # -------------------------------------------------------------------------------------------
 # xLSTM-TS logic
@@ -15,13 +16,17 @@ from ml.models.xlstm_ts.training import train_model, evaluate_model
 
 def run_xlstm_ts(train_x, train_y, val_x, val_y, test_x, test_y, scaler, stock, data_type, test_dates, train_y_original=None, val_y_original=None, test_y_original=None):
     xlstm_stack, input_projection, output_projection = create_xlstm_model(SEQ_LENGTH_XLSTM)
-
     xlstm_stack, input_projection, output_projection = train_model(xlstm_stack, input_projection, output_projection, train_x, train_y, val_x, val_y)
+
+    scaler_open = scaler[0]
+    scaler_close = scaler[1]
 
     test_predictions = evaluate_model(xlstm_stack, input_projection, output_projection, test_x)
 
     # Invert the normalisation for comparison
-    test_predictions = inverse_normalise_data_xlstm(test_predictions.squeeze(), scaler)
+    test_predictions_open = inverse_normalise_data_xlstm(test_predictions[:, 0].squeeze(), scaler_open)
+    test_predictions_close = inverse_normalise_data_xlstm(test_predictions[:, 1].squeeze(), scaler_close)
+    test_predictions = np.column_stack((test_predictions_open, test_predictions_close))
 
     # If the original data is provided, use it for the evaluation
     if train_y_original is not None and val_y_original is not None and test_y_original is not None:
@@ -29,7 +34,9 @@ def run_xlstm_ts(train_x, train_y, val_x, val_y, test_x, test_y, scaler, stock, 
         val_y = val_y_original
         test_y = test_y_original
     
-    test_y = inverse_normalise_data_xlstm(test_y, scaler)
+    test_y_open = inverse_normalise_data_xlstm(test_y[:, 0], scaler_open)
+    test_y_close = inverse_normalise_data_xlstm(test_y[:, 1], scaler_close)
+    test_y = np.column_stack((test_y_open, test_y_close))
 
     model_name = 'xLSTM-TS'
     metrics_price = calculate_metrics(test_y, test_predictions, model_name, data_type)
@@ -41,19 +48,24 @@ def run_xlstm_ts(train_x, train_y, val_x, val_y, test_x, test_y, scaler, stock, 
     val_predictions = evaluate_model(xlstm_stack, input_projection, output_projection, val_x)
 
     # Invert the normalisation for comparison
-    train_predictions = inverse_normalise_data_xlstm(train_predictions.squeeze(), scaler)
-    train_y = inverse_normalise_data_xlstm(train_y, scaler)
+    train_predictions_open = inverse_normalise_data_xlstm(train_predictions[:, 0].squeeze(), scaler_open)
+    train_predictions_close = inverse_normalise_data_xlstm(train_predictions[:, 1].squeeze(),scaler_close)
+    train_predictions = np.column_stack((train_predictions_open, train_predictions_close))
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    val_predictions = inverse_normalise_data_xlstm(val_predictions.squeeze(), scaler)
-    val_y = inverse_normalise_data_xlstm(val_y, scaler)
+    train_y_open = inverse_normalise_data_xlstm(train_y[:, 0], scaler_open)
+    train_y_close = inverse_normalise_data_xlstm(train_y[:, 1], scaler_close)
+    train_y = np.column_stack((train_y_open, train_y_close))
 
-    true_labels, predicted_labels, metrics_direction = evaluate_directional_movement(train_y, train_predictions, val_y, val_predictions, test_y, test_predictions, model_name, data_type, using_darts=False)
-=======
+
+    val_predictions_open = inverse_normalise_data_xlstm(val_predictions[:, 0].squeeze(), scaler_open)
+    val_predictions_close = inverse_normalise_data_xlstm(val_predictions[:, 1].squeeze(),scaler_close)
+    val_predictions = np.column_stack((val_predictions_open, val_predictions_close))
+
+    val_y_open = inverse_normalise_data_xlstm(val_y[:, 0], scaler_open)
+    val_y_close = inverse_normalise_data_xlstm(val_y[:, 1], scaler_close)
+    val_y = np.column_stack((val_y_open, val_y_close))
+
     true_labels, predicted_labels, metrics_direction, predictions_df = evaluate_directional_movement(train_y, train_predictions, val_y, val_predictions, test_y, test_predictions, model_name, data_type, using_darts=False)
->>>>>>> Stashed changes
 
     metrics_price.update(metrics_direction)
 
@@ -66,8 +78,6 @@ def run_xlstm_ts(train_x, train_y, val_x, val_y, test_x, test_y, scaler, stock, 
         'Predicted Label': predicted_labels.tolist()
     }
 
-<<<<<<< Updated upstream
-=======
     data = {
         'Date': test_dates.tolist(),
         'Open': [y[0] for y in test_y],  
@@ -76,80 +86,9 @@ def run_xlstm_ts(train_x, train_y, val_x, val_y, test_x, test_y, scaler, stock, 
         'Predicted close': [p[1] for p in test_predictions],
     }
     
->>>>>>> Stashed changes
     results_df = pd.DataFrame(data)
 
     return results_df, metrics_price, predictions_df
-
-
-def run_xlstm_ts_1out(train_x, train_y, val_x, val_y, test_x, test_y, scaler, stock, data_type, test_dates, train_y_original=None, val_y_original=None, test_y_original=None):
-    xlstm_stack, input_projection, output_projection = create_xlstm_model(SEQ_LENGTH_XLSTM)
-    xlstm_stack, input_projection, output_projection = train_model(xlstm_stack, input_projection, output_projection, train_x, train_y, val_x, val_y)
-
-    test_predictions = evaluate_model(xlstm_stack, input_projection, output_projection, test_x)
-
-    # Invert the normalisation for comparison
-    test_predictions = inverse_normalise_data_xlstm(test_predictions.squeeze(), scaler)
-
-
-    # If the original data is provided, use it for the evaluation
-    if train_y_original is not None and val_y_original is not None and test_y_original is not None:
-        train_y = train_y_original
-        val_y = val_y_original
-        test_y = test_y_original
-    
-    test_y = inverse_normalise_data_xlstm(test_y, scaler)
-
-    model_name = 'xLSTM-TS'
-    metrics_price = calculate_metrics(test_y, test_predictions, model_name, data_type)
-
-    visualise(test_y, test_predictions, stock, model_name, data_type, show_complete=True, dates=test_dates)
-    visualise(test_y, test_predictions, stock, model_name, data_type, show_complete=False, dates=test_dates)
-
-    train_predictions = evaluate_model(xlstm_stack, input_projection, output_projection, train_x)
-    val_predictions = evaluate_model(xlstm_stack, input_projection, output_projection, val_x)
-
-    # Invert the normalisation for comparison
-    train_predictions = inverse_normalise_data_xlstm(train_predictions.squeeze(), scaler)
-
-    train_y = inverse_normalise_data_xlstm(train_y, scaler)
-
-    val_predictions = inverse_normalise_data_xlstm(val_predictions.squeeze(), scaler)
-
-    val_y = inverse_normalise_data_xlstm(val_y, scaler)
-
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-    true_labels, predicted_labels, metrics_direction, predictions_df = evaluate_directional_movement(train_y, train_predictions, val_y, val_predictions, test_y, test_predictions, model_name, data_type, using_darts=False)
-
-    metrics_price.update(metrics_direction)
-
-    # Combine data into a DataFrame
-    data = {
-        'Date': test_dates.tolist()[:-1],
-        'Close': [item for sublist in test_y for item in sublist][:-1],
-        'Predicted Value': [item for sublist in test_predictions for item in sublist][:-1],
-        'True Label': true_labels.tolist(),
-        'Predicted Label': predicted_labels.tolist()
-    }
-
-    data = {
-        'Date': test_dates.tolist(),
-        'Value': [y for y in test_y],  
-        'Predicted Open': [p for p in test_predictions]
-    }
-    
-    results_df = pd.DataFrame(data)
-
-    return results_df, metrics_price, predictions_df
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
 
 
 def run_xlstm_ts_1out(train_x, train_y, val_x, val_y, test_x, test_y, scaler, stock, data_type, test_dates, train_y_original=None, val_y_original=None, test_y_original=None):
@@ -211,7 +150,3 @@ def run_xlstm_ts_1out(train_x, train_y, val_x, val_y, test_x, test_y, scaler, st
     results_df = pd.DataFrame(data)
 
     return results_df, metrics_price, predictions_df
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
